761df575255c630902e981b380943e8d219544b3
MarcialFantini <marcialfantini@gmail.com>
2024-05-30 04:45:52 -0300

    upgrade(upgrade system of load)

--- src/components/Skills/index.tsx
+++ src/components/Skills/index.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { skills } from "./skills";
 import { Tooltip } from "@nextui-org/react";
 import srcFlower from "../../../public/image/flower.png";
+import { Suspense } from "react";
 
 export const Skills = () => {
   return (
@@ -10,17 +11,19 @@ export const Skills = () => {
       className="  bg-gradient-to-r from-[rgb(244,114,182,0.4)] p-[40px] rounded-lg relative"
     >
       <picture className=" absolute z-[-1] h-full bottom-0 right-0 ">
-        <Image
-          className=" object-contain w-full h-full "
-          src={srcFlower}
-          alt="flower"
-        ></Image>
+        <Suspense>
+          <Image
+            className=" object-contain w-full h-full "
+            src={srcFlower}
+            alt="flower"
+          ></Image>
+        </Suspense>
       </picture>
       <h2
         style={{ textShadow: "purple 0px 0px 10px" }}
         className=" text-center text-white text-4xl my-12 shadow-lg"
       >
-   My technical skills
+        My technical skills
       </h2>
       <section
         style={{ gridTemplateColumns: "repeat(auto-fit,minmax(80px,1fr))" }}
@@ -28,15 +31,17 @@ export const Skills = () => {
       >
         {skills.map((skill, index) => {
           return (
-            <Tooltip key={index + skill.title} content={skill.title}>
-              <picture className=" border-white border-x-2 border-y-2 cursor-pointer flex items-center justify-center aspect-square max-h-[100px] bg-[rgb(10,25,59)] p-4 rounded-2xl transition-all hover:rotate-[25deg]">
-                <Image
-                  className=" w-full h-full object-cover"
-                  src={skill.src}
-                  alt={skill.title}
-                ></Image>
-              </picture>
-            </Tooltip>
+            <Suspense key={index + skill.title}>
+              <Tooltip key={index + skill.title} content={skill.title}>
+                <picture className=" border-white border-x-2 border-y-2 cursor-pointer flex items-center justify-center aspect-square max-h-[100px] bg-[rgb(10,25,59)] p-4 rounded-2xl transition-all hover:rotate-[25deg]">
+                  <Image
+                    className=" w-full h-full object-cover"
+                    src={skill.src}
+                    alt={skill.title}
+                  ></Image>
+                </picture>
+              </Tooltip>
+            </Suspense>
           );
         })}
       </section>
--- src/components/Works/index.tsx
+++ src/components/Works/index.tsx
@@ -6,44 +6,46 @@ import { Suspense } from "react";
 
 export const Works = () => {
   return (
-    <section
-      id="Trabajos"
-      className=" w-[90%] max-w-[1200px] p-[40px] mx-auto my-[50px] bg-gradient-to-l from-[rgb(20,184,166,0.4)] rounded-lg"
-    >
-      <h2
-        style={{ textShadow: "0px 0px 20px purple" }}
-        className=" text-4xl text-white text-center m-[40px] "
+    <Suspense>
+      <section
+        id="Trabajos"
+        className=" w-[90%] max-w-[1200px] p-[40px] mx-auto my-[50px] bg-gradient-to-l from-[rgb(20,184,166,0.4)] rounded-lg"
       >
-        My recent projects
-      </h2>
-      <section className=" grid lg:grid-cols-4 gap-4">
-        {projects.map((project, index) => {
-          return (
-            <Link
-              target="_blank"
-              href={project.link}
-              key={project.title + index}
-              className=" "
-            >
-              <article className="  border-black border-x-2 border-y-2 relative flex items-start justify-start overflow-hidden rounded-2xl">
-                <h4 className=" bottom-0 absolute p-4 bg-[rgb(0,0,0)] backdrop-blur-sm w-full text-center z-10 text-base">
-                  {project.title}
-                </h4>
+        <h2
+          style={{ textShadow: "0px 0px 20px purple" }}
+          className=" text-4xl text-white text-center m-[40px] "
+        >
+          My recent projects
+        </h2>
+        <section className=" grid lg:grid-cols-4 gap-4">
+          {projects.map((project, index) => {
+            return (
+              <Link
+                target="_blank"
+                href={project.link}
+                key={project.title + index}
+                className=" "
+              >
+                <article className="  border-black border-x-2 border-y-2 relative flex items-start justify-start overflow-hidden rounded-2xl">
+                  <h4 className=" bottom-0 absolute p-4 bg-[rgb(0,0,0)] backdrop-blur-sm w-full text-center z-10 text-base">
+                    {project.title}
+                  </h4>
 
-                <picture className=" max-h-[600px] overflow-hidden w-full flex justify-start items-start">
-                  <Suspense>
-                    <Image
-                      className="w-full h-full object-cover "
-                      src={project.img}
-                      alt={project.title}
-                    ></Image>
-                  </Suspense>
-                </picture>
-              </article>
-            </Link>
-          );
-        })}
+                  <picture className=" max-h-[600px] overflow-hidden w-full flex justify-start items-start">
+                    <Suspense>
+                      <Image
+                        className="w-full h-full object-cover "
+                        src={project.img}
+                        alt={project.title}
+                      ></Image>
+                    </Suspense>
+                  </picture>
+                </article>
+              </Link>
+            );
+          })}
+        </section>
       </section>
-    </section>
+    </Suspense>
   );
 };
